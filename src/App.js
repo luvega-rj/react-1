845afeba9b5c8dd4195cfa2131b3ae3eb0807954
MORGANJASON <420morganjason@gmail.com>
2023-04-18 13:32:34 +0300

    completed image

--- src/App.js
+++ src/App.js
@@ -3,14 +3,60 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Friend'
 
+
+// function App() {
+// return (
+// <div>
+// <h1>Find A Person </h1>
+// <img
+// className="App-img" 
+// src="https://images.unsplash.com/photo-1510784722466-f2aa9c52fff6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" 
+// alt="imagess" 
+// />
+// </div>
+// );
+// }
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Greet />
+        <h1>Find A Person </h1>
+<img
+className="App-img" 
+src="https://images.unsplash.com/photo-1510784722466-f2aa9c52fff6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" 
+alt="imagess" 
+/>
+
       </div>
+      
+
     );
   }
 }
 
 export default App;
+
+
+
+
+
+
+// import Location from './components/Location'
+
+// function App() {
+// return (
+// <div>
+// <h1>Find A Person </h1>
+// <img
+// className="App-img" 
+// src="https://images.unsplash.com/photo-1510784722466-f2aa9c52fff6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" 
+// alt="imagess" 
+// />
+// </div>
+// );
+// }
+
+// export default App
+
