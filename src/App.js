f6b52891a0c354578aa0b5aa40a736a1b4e7d486
Charles Njoroge <charles.maina@student.moringaschool.com>
2023-04-18 14:28:23 +0300

    styled

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
         <h1>Find A Person </h1>
 <img
 className="App-img" 
-src="https://images.unsplash.com/photo-1510784722466-f2aa9c52fff6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" 
+src="https://images.unsplash.com/photo-1644994474943-6918220e745b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTZ8ODY0MDQyfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" 
 alt="imagess" 
 />
 
